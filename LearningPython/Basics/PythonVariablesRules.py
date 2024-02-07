7c5c0eaa781f21ccd75fe1c5efcb47456d25f327
nabilahaffryda <sehungantengs@gmail.com>
2024-02-07 14:21:51 +0700

    Initial commit

---
+++ LearningPython/Basics/PythonVariablesRules.py
@@ -0,0 +1,17 @@
+import keyword
+keywordlist = keyword.kwlist
+print(keywordlist)
+print(keyword.iskeyword("try"))
+tryyu = "testing"
+_test = "hello world"
+rba2_test=908
+print(_test)
+print(rba2_test)
+
+RBA2_test=7878
+rba2_test=13829
+
+print(rba2_test)
+print(RBA2_test)
+
+
---
+++ LearningPython/Basics/Variables.py
@@ -0,0 +1,12 @@
+username=12.98
+print(username)
+
+print(type(username))
+
+x=600
+print(x)
+y=900
+print(y)
+print(id(x))
+print(id(y))
+
