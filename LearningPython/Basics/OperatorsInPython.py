912eccfa0f4b30dce013a75daccd860d52f070f1
nabilahaffryda <sehungantengs@gmail.com>
2024-03-06 10:04:49 +0700

    operator in python

---
+++ LearningPython/Basics/OperatorsInPython.py
@@ -0,0 +1,61 @@
+# Arithmetic operators
+# x = 15
+# y = 4
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# print(x // y)
+
+# Assignment operators
+# x = 10
+# x += 2
+# print(x)
+# x -= 4
+# print(x)
+# x *= 4
+# print(x)
+# x /= 4
+# print(x)
+# x **= 4
+# print(x)
+
+# Comparison operators
+# x = 10
+# y = 6
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+
+# Logical operators
+# x = 10
+# y = 4
+# print(x == y and x > y)
+# print(x == y or x > y)
+
+# Identity operators
+# x = ["python", "pyt"]
+# y = ["python", "pyt"]
+# print(x is y)
+# print(x is not y)
+
+# Membership operators
+# x = ["python", "pyt"]
+# y = ["python", "pyt"]
+# print("pyt" in y)
+# print("pyt" not in y)
+
+# Bitwise operators
+# 0 - 00
+# 1 - 01
+# 2 - 10
+# 3 - 11
+x = 1
+y = 3
+print(x & y)
+print(x | y)
